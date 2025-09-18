be0be7d8236429707dacb7d222f6322b32189d55
dominiofast <92938911+dominiofast@users.noreply.github.com>
2025-09-18 15:21:59 -0400

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -38,15 +38,15 @@ type Action =
   | {
       type: ActionType["ADD_TOAST"]
       toast: ToasterToast
-
+    }
   | {
       type: ActionType["UPDATE_TOAST"]
       toast: Partial<ToasterToast>
-
+    }
   | {
       type: ActionType["DISMISS_TOAST"]
       toastId?: ToasterToast["id"]
-
+    }
   | {
       type: ActionType["REMOVE_TOAST"]
       toastId?: ToasterToast["id"]
